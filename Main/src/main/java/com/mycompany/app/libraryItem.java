b8a3fcc016aede7e1f385504d4b59697b160c462
RareMandolin <dantewalden1@gmail.com>
2025-04-25 16:11:23 -0400

    Capitalized LibraryItem.java

--- Main/src/main/java/com/mycompany/app/libraryItem.java
+++ Main/src/main/java/com/mycompany/app/libraryItem.java
@@ -2,20 +2,20 @@ package com.mycompany.app;
 
 import java.time.LocalDate;
 
-public class libraryItem {
+public class LibraryItem {
     private String name;
     private User creator;
     private double length;
     private LocalDate creationDate;
 
-    public libraryItem(String name, User creator, double length) {
+    public LibraryItem(String name, User creator, double length) {
         this.name = name;
         this.creator = creator;
         this.length = length;
         creationDate = LocalDate.now();
     }
 
-    public libraryItem(String name, User creator, double length, LocalDate creationDate) {
+    public LibraryItem(String name, User creator, double length, LocalDate creationDate) {
         this.name = name;
         this.creator = creator;
         this.length = length;
